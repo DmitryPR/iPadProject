f0c1a13b5ed6bcc58699e56fab156bfa8e75051d
Dmitry Preobrazhenskiy <d.preobrazhenskiy@gmail.com>
2012-01-04 16:28:54 +0200

    Initial commit

---
+++ SiSIpad/SiSIpad/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  SiSIpad
+//
+//  Created by Dmitry Preobrazhenskiy on 04.01.12.
+//  Copyright (c) 2012 TTU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController <UISplitViewControllerDelegate>
+
+@property (strong, nonatomic) id detailItem;
+
+@property (strong, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
---
+++ SiSIpad/SiSIpad/MasterViewController.h
@@ -0,0 +1,22 @@
+//
+//  MasterViewController.h
+//  SiSIpad
+//
+//  Created by Dmitry Preobrazhenskiy on 04.01.12.
+//  Copyright (c) 2012 TTU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+#import <CoreData/CoreData.h>
+
+@interface MasterViewController : UITableViewController <NSFetchedResultsControllerDelegate>
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+@end
